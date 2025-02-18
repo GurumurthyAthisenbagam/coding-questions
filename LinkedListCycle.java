94f193539bc7452372847570f64203fcd28854ce
phoenixmysoul <132504277+phoenixmysoul@users.noreply.github.com>
2025-02-17 17:13:27 -0800

    Create LinkedListCycle.java

---
+++ LinkedListCycle.java
@@ -0,0 +1,43 @@
+/**
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+**/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fastNode = head;
+        ListNode slowNode = head;
+
+        int counter =0;
+
+
+        while (fastNode!=null && fastNode.next != null) {
+
+            counter ++;
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+
+        }
+
+    return slowNode;
+    }
+}
