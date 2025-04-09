9eb1541caaa36051cb1d6383bf8f648f7c2c4b11
Gurumurthy Athisenbagam <132504277+GurumurthyAthisenbagam@users.noreply.github.com>
2025-04-09 15:58:22 -0700

    Create LC1060-MissingElementSortedArray.java

---
+++ medium/LC1060-MissingElementSortedArray.java
@@ -0,0 +1,29 @@
+/**
+Between nums[i] and nums[i-1]:
+Missing numbers = nums[i] - nums[i-1] -1;
+
+✅ Why?
+nums[j] - nums[i] = total distance
+j - i = how many numbers are actually present
+Subtract to get missing numbers.
+
+Go through the array
+Keep subtracting missing counts until you find where the k-th missing number lies.
+
+ */
+
+class Solution {
+    public int missingElement(int[] nums, int k) {
+
+        for (int i=1 ; i<nums.length; i++) {
+
+            int missingNumbers = nums[i] - nums[i-1] -1;
+            if (k <= missingNumbers) { // k is within range, so find lowest number and add k to it.
+                return nums[i-1]+k;
+            }
+            k = k - missingNumbers;
+        }
+        
+        return nums[nums.length-1] + k; // didnt find k in the array. k is after the last element. 
+    }
+}
