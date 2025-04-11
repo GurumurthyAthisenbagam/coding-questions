15e20bc81150c3e2f3f1b38134a4ffa1793a25a1
Gurumurthy Athisenbagam <132504277+GurumurthyAthisenbagam@users.noreply.github.com>
2025-04-11 12:36:41 -0700

    Create LC1768-MergeStringsAlternately.java

---
+++ LC1768-MergeStringsAlternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+        StringBuilder str = new StringBuilder();
+        int i=0, j=0;
+        for (; i<word1.length() && j<word2.length(); i++, j++) {
+            str.append(word1.charAt(i));
+            str.append(word2.charAt(j));
+        }
+
+        while (i<word1.length()) {
+            str.append(word1.charAt(i));
+            i++;
+        }
+        while (j<word2.length()) 
+        {
+            str.append(word2.charAt(j));
+            j++;
+        }
+
+        return str.toString();
+        
+    }
+}
