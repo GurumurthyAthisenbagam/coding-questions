2a4a26ad49b09635486fe56d5eca84b30e8de43c
Gurumurthy Athisenbagam <132504277+GurumurthyAthisenbagam@users.noreply.github.com>
2025-04-26 16:18:19 -0700

    Update LC824-GoatLatin.java

--- LC824-GoatLatin.java
+++ LC824-GoatLatin.java
@@ -1,4 +1,38 @@
-class Solution {
+public class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] words = sentence.split(" ");
+        String vowels = "aeiouAEIOU";
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            
+            if (vowels.indexOf(word.charAt(0)) >= 0) {
+                result.append(word);
+            } else {
+                result.append(word.substring(1)).append(word.charAt(0));
+            }
+            
+            result.append("ma");
+            
+            // Append (i+1) 'a's
+            for (int j = 0; j <= i; j++) {
+                result.append('a');
+            }
+            
+            if (i != words.length - 1) {
+                result.append(' ');
+            }
+        }
+        
+        return result.toString();
+    }
+}
+
+
+
+/** class Solution {
     public String toGoatLatin(String sentence) {
         String[] words =  sentence.split(" ");
 
@@ -27,3 +61,5 @@ class Solution {
         return false;
     }
 }
+
+**/
